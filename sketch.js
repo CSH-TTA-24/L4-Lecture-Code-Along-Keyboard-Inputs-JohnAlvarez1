22741f524e5a99235b610e69c8001d839c580053
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 09:54:27 -0400

    Initial Commit

---
+++ sketch.js
@@ -0,0 +1,39 @@
+let xPos = 250
+let yPos = 250
+
+
+function setup() {
+  createCanvas(500, 500);
+  noStroke();
+  
+  rectMode(CENTER);
+}
+
+function draw() {
+  background(0);
+rect(xPos, yPos, 50, 50,)
+fill('red')
+}
+
+
+if(keyIsDown(UP_ARROW){
+  yPos -= 3 * direction;
+}
+
+if(keyIsDown(DOWN_ARROW)){
+  yPos += 3 * direction;
+
+
+}
+
+if(keyIsDown(UP_ARROW)){
+  yPos -= 3 * direction;
+
+
+}
+
+if(keyIsDown(UP_ARROW)){
+  yPos -= 3 * direction;
+
+
+}
